4cbf7b3cc75a0a2f1ccd49eba21c46e6bf6dad8a
Asteip <abonnin44@gmail.com>
2016-11-30 15:55:12 +0100

    Modification de la version de java : changement de 1.8 en 1.7. Ajout de l'api pour les questions.

--- TheKnowledgeStory/src/api/QuestionEntity.java
+++ TheKnowledgeStory/src/api/QuestionEntity.java
@@ -1,7 +1,5 @@
 package api;
 
-import java.util.ArrayList;
-
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
@@ -12,30 +10,80 @@ import javax.jdo.annotations.PrimaryKey;
 public class QuestionEntity {
 	@PrimaryKey
 	@Persistent(valueStrategy=IdGeneratorStrategy.IDENTITY)
-	String question;
+	Long id;
 
 	@Persistent
-	String reponseBonne;
+	String theme;
 	
 	@Persistent
-	ArrayList<String> reponsesMauvaises;
+	String question;
 	
-	/*public Long getId() {
+	@Persistent
+	String answer;
+	
+	@Persistent
+	String wrongAnswer1;
+	
+	@Persistent
+	String wrongAnswer2;
+	
+	@Persistent
+	String wrongAnswer3;
+
+	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+
+	public String getTheme() {
+		return theme;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setTheme(String theme) {
+		this.theme = theme;
 	}
-	public int getScore() {
-		return score;
+
+	public String getQuestion() {
+		return question;
 	}
-	public void setScore(int score) {
-		this.score = score;
-	}*/
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public String getWrongAnswer1() {
+		return wrongAnswer1;
+	}
+
+	public void setWrongAnswer1(String wrongAnswer1) {
+		this.wrongAnswer1 = wrongAnswer1;
+	}
+
+	public String getWrongAnswer2() {
+		return wrongAnswer2;
+	}
+
+	public void setWrongAnswer2(String wrongAnswer2) {
+		this.wrongAnswer2 = wrongAnswer2;
+	}
+
+	public String getWrongAnswer3() {
+		return wrongAnswer3;
+	}
+
+	public void setWrongAnswer3(String wrongAnswer3) {
+		this.wrongAnswer3 = wrongAnswer3;
+	}
+	
 }
