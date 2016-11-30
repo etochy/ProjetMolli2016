43438a035f6799805bad2ae80d52e8f9bae53f4b
RoyalAkuma <residencechampsdecanne@msn.com>
2016-11-30 16:29:36 +0100

    Servlet DoGet DoPost

--- TheKnowledgeStory/src/servlets/MainServlet.java
+++ TheKnowledgeStory/src/servlets/MainServlet.java
@@ -3,42 +3,72 @@ package servlets;
 import java.io.IOException;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.EmbeddedEntity;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.KeyRange;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class MainServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		
 		
 		//creation des entités question dans le datastore :
 		//https://cloud.google.com/appengine/docs/java/datastore/entities
-		
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		
-		Entity question = new Entity("Question");
-		question.setProperty("Theme",  "Histoire");
-		question.setProperty("question", "Une jolie question ?");
-		question.setProperty("réponse", "La bonne réponse");
-		question.setProperty("f1", "la premiere mauvaise réponse");
-		question.setProperty("f2", "la seconde mauvaise réponse");
-		question.setProperty("f3", "la troisième mauvaise réponse");
-		datastore.put(question);
-		
-		//httpRequest
+		//try {
+			
+		//	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		
 		
+		// Utile pour faire les highscore
+		//------------------------------------------
+		// Demande tous les 5 derniers messages triés par date décroissante
+			
+		//Query q = new Query("Question").addSort("question", SortDirection.DESCENDING);
+        //List<Entity> results = datastore.prepare(q).asList(FetchOptions.Builder.withLimit(5));
+		//req.setAttribute("messages", results);
+        //this.getServletContext().getRequestDispatcher("/WEB-INF/guestbook.jsp").forward(req, resp);
+
+		//} catch (ServletException e) {
+
+        //e.printStackTrace();
+
+		//} catch (IOException e) {
+
+         //e.printStackTrace();
+
+		//}
+	//}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {  
+
+	     try {
+
+	            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+
+	            // Stocke le une question
+
+				Entity question = new Entity("Question");
+				question.setProperty("Theme",  "Histoire");
+				question.setProperty("question", "Une jolie question ?");
+				question.setProperty("réponse", "La bonne réponse");
+				question.setProperty("f1", "la premiere mauvaise réponse");
+				question.setProperty("f2", "la seconde mauvaise réponse");
+				question.setProperty("f3", "la troisième mauvaise réponse");
+				datastore.put(question);
+
+
+	            resp.sendRedirect("/");
+
+	        } catch (IOException e) {
+
+	            e.printStackTrace();
+
+	        }
+
+	    }		
 		
-		
-		
-		
-		
-	}
 }
+
+
