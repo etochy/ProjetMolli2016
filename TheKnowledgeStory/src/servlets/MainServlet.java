202d93091ef5816369733bcddbac7584d987f873
RoyalAkuma <residencechampsdecanne@msn.com>
2016-11-30 17:08:57 +0100

    update

--- TheKnowledgeStory/src/servlets/MainServlet.java
+++ TheKnowledgeStory/src/servlets/MainServlet.java
@@ -1,44 +1,52 @@
 package servlets;
 
-import java.io.IOException;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-
-import javax.servlet.ServletException;
+import java.io.*;
+import java.util.* ;
+import javax.servlet.* ;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class MainServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
-		
-		
-		//creation des entités question dans le datastore :
-		//https://cloud.google.com/appengine/docs/java/datastore/entities
-		//try {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+		}
+	public  void doPost(HttpServletRequest request, HttpServletResponse response)
+			 throws ServletException, IOException  {
 			
-		//	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+				doGet(request, response) ;
+			}
+		}	
+
+	
+	/*
+	creation des entités question dans le datastore :
+	https://cloud.google.com/appengine/docs/java/datastore/entities
+	try {
+			
+	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		
 		
-		// Utile pour faire les highscore
+		//Utile pour faire les highscore
 		//------------------------------------------
 		// Demande tous les 5 derniers messages triés par date décroissante
 			
-		//Query q = new Query("Question").addSort("question", SortDirection.DESCENDING);
-        //List<Entity> results = datastore.prepare(q).asList(FetchOptions.Builder.withLimit(5));
-		//req.setAttribute("messages", results);
-        //this.getServletContext().getRequestDispatcher("/WEB-INF/guestbook.jsp").forward(req, resp);
+		Query q = new Query("Question").addSort("question", SortDirection.DESCENDING);
+        List<Entity> results = datastore.prepare(q).asList(FetchOptions.Builder.withLimit(5));
+		req.setAttribute("messages", results);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/guestbook.jsp").forward(req, resp);
 
-		//} catch (ServletException e) {
+		} catch (ServletException e) {
 
-        //e.printStackTrace();
+        e.printStackTrace();
 
-		//} catch (IOException e) {
+		} catch (IOException e) {
 
-         //e.printStackTrace();
+         e.printStackTrace();
 
-		//}
-	//}
+		}
+	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) {  
 
@@ -47,7 +55,7 @@ public class MainServlet extends HttpServlet {
 	            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
 
-	            // Stocke le une question
+	            // Stocke une question
 
 				Entity question = new Entity("Question");
 				question.setProperty("Theme",  "Histoire");
@@ -69,6 +77,7 @@ public class MainServlet extends HttpServlet {
 
 	    }		
 		
-}
+	 */
+
 
 
