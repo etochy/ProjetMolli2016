39e98eaacf99217b6875db6a22e3a6758485cfa9
RoyalAkuma <residencechampsdecanne@msn.com>
2016-11-30 14:46:08 +0100

    retest config

--- ProjetMolli/src/projetmolli/ProjetMolliServlet.java
+++ ProjetMolli/src/projetmolli/ProjetMolliServlet.java
@@ -1,12 +1,11 @@
-package projetQuizz;
+package projetmolli;
 
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class ProjetMolliSem1Servlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+public class ProjetMolliServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
