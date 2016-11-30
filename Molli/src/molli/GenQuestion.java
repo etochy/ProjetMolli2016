7f00f870af8a6087d58f5b189744109965995ce0
RoyalAkuma <residencechampsdecanne@msn.com>
2016-11-30 13:25:06 +0100

    ajout des fichiers d'api d'Alexis

---
+++ Molli/src/molli/GenQuestion.java
@@ -0,0 +1,26 @@
+package projetQuizz;
+
+public class GenQuestion {
+
+	public String question;
+	public String sujet;
+	public String type;
+	
+    public void GenQuestion(){
+
+      switch (type) {
+              case "ou":
+                question = "Où a eu lieu " + sujet + " ?" ;
+                break;
+              case "quand":
+                question = "Quand a eu lieu " + sujet + " ?" ;  
+                break;
+              case "qui":
+                question = "Qui a participé à " +  sujet + " ?";
+                break;
+              default: question = "Erreur";
+                       break;
+          }
+          System.out.println(question);
+      }
+}
