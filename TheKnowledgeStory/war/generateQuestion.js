a822662c89380bbb99a393f0a623ce69e8a6224a
etochy <etochy@gmail.com>
2016-12-22 05:57:12 -0800

    test parse json

--- TheKnowledgeStory/war/generateQuestion.js
+++ TheKnowledgeStory/war/generateQuestion.js
@@ -1,15 +1,14 @@
 (function(){
 	var app = angular.module('genQ',[]);
-	
+
 	app.controller('customerCtrl', function($scope, $http){
-		$http.get().then(function(response){
+		$http.get('batailles.json').then(function(response){
 			$scope.myData = response.data.results;
+			
+			for(var i=0; i<$scope.myData.length; i++){
+				console.log($scope.myData[i].nomlabel);
+			}
+			
 		});
 	});
-
-
-
-
-
-})();
 
