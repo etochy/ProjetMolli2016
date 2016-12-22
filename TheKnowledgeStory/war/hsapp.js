f13c1a611a6aac1ad09b769b104ce76ed51c8798
RoyalAkuma <residencechampsdecanne@msn.com>
2016-12-23 00:42:35 +0100

    highscore almost done

--- TheKnowledgeStory/war/hsapp.js
+++ TheKnowledgeStory/war/hsapp.js
@@ -1,10 +1,12 @@
 (function(){
 	var app = angular.module('highscore',[]);
-	  app.controller('ScoreController', ['$scope','$window', function($scope, $window){
-		    
-	  $window.init = function(){
-		  var rootApi = 'http://1-dot-theknowledgeknight.appspot.com/_ah/api/';
-		  gapi.client.load('scoreentityendpoint','v1', function(){
+	  app.controller('ScoreController', ['$scope','$window', function($scope,$window){
+		  		$scope.scores =[];
+		  	
+	$window.init = function(){
+		  var rootApi = 'https://1-dot-theknowledgestory.appspot.com/_ah/api/';
+		  
+		  gapi.client.load('scoreentityendpoint', 'v1', function() {
 			  gapi.client.scoreentityendpoint.listScoreEntity().execute(
 	  					function(resp) {
 				  $scope.scores = resp.items;
